d270ca275326c65f131423401ad50917d9cbc430
leedasom050115 <kiss9892@inha.edu>
2025-05-26 14:22:40 +0900

    과제제출

--- src/components/Darkmode/Darkmode.jsx
+++ src/components/Darkmode/Darkmode.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import "./Darkmode.css";
 const Darkmode = () => {
-  return <div></div>;
+  const [darkmode, setDarkmode] = useState(false);
+
+
+  return <div>
+    <p>라이트모드</p>
+    <button onclick = {setDarkmode}>다크모드로 변경</button>
+  </div>;
 };
 
 export default Darkmode;
